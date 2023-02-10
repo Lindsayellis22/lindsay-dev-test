899df1dde3b15ff1eb2b753eeb8cd91eb44b7d94
lindsayellis22 <lindsayellis39@gmail.com>
2023-02-10 14:39:08 -0500

    aligned body text

--- src/app.js
+++ src/app.js
@@ -9,17 +9,26 @@ function App(props) {
 		fontWeight: "bold",
 	};
 	return (
-		<header className="hero-image">
-			<div className="hero-text">
-				<h1 className="animate__animated animate__fadeIn animate__slower">
-					WELCOME TO
-				</h1>
-				<h2>Rooster Grin</h2>
-				<Button variant="contained" style={style}>
-					Learn More
-				</Button>
+		<>
+			<header className="hero-image">
+				<div className="hero-text">
+					<h1 className="animate__animated animate__fadeIn animate__slower">
+						WELCOME TO
+					</h1>
+					<h2 className="hero-second-header">Rooster Grin</h2>
+					<Button variant="contained" style={style}>
+						Learn More
+					</Button>
+				</div>
+			</header>
+			<div className="body-content">
+				<div className="body-text">
+					<h2>LOREM IPSUM</h2>
+					<p>lorem ipsum jdjdjdjd</p>
+				</div>
+				<img className="img-1" alt="climbing" />
 			</div>
-		</header>
+		</>
 	);
 }
 
