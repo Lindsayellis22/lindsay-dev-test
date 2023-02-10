729cd1fd97c40b5ae4d13ef939d7828b00c4da8c
lindsayellis22 <lindsayellis39@gmail.com>
2023-02-10 16:45:24 -0500

    added text toggle

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import { Button } from "@material-ui/core";
 import "animate.css";
+import { useState } from "react";
 
 function App(props) {
+	const [showMore, setShowMore] = useState(false);
 	const style = {
 		backgroundColor: "purple",
 		color: "white",
@@ -24,9 +26,32 @@ function App(props) {
 			<div className="body-content">
 				<div className="body-text">
 					<h2>LOREM IPSUM</h2>
-					<p>lorem ipsum jdjdjdjd</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Magnis
+						dis parturient montes nascetur ridiculus mus mauris. Ullamcorper
+						morbi tincidunt ornare massa eget egestas purus. Quis eleifend quam
+						adipiscing vitae proin. Diam donec adipiscing tristique risus nec
+						feugiat. Ipsum dolor sit amet consectetur adipiscing. Ornare aenean
+						euismod elementum nisi. Nunc scelerisque viverra mauris in aliquam
+						sem.
+					</p>
+					{showMore ? (
+						<p>
+							Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt.
+							Habitasse platea dictumst quisque sagittis. Vulputate eu
+							scelerisque felis imperdiet proin fermentum leo. Turpis egestas
+							maecenas pharetra convallis posuere morbi leo urna. Eu consequat
+							ac felis donec et odio. Integer malesuada nunc vel risus commodo
+							viverra maecenas.
+						</p>
+					) : null}
+					<Button onClick={() => setShowMore(!showMore)}>> Learn More</Button>
 				</div>
 				<img className="img-1" alt="climbing" />
+			</div>
+			<div>
+				<img className="banner-2" alt="computer" />
 			</div>
 		</>
 	);
