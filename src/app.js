aeccbb8373b6df8100e4dd432587cb346b551dfd
lindsayellis22 <lindsayellis39@gmail.com>
2023-02-10 13:15:36 -0500

    add button and animation

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 import "./App.css";
+import { Button } from "@material-ui/core";
+import "animate.css";
 
-function App() {
+function App(props) {
+	const style = {
+		backgroundColor: "purple",
+		color: "white",
+		fontWeight: "bold",
+	};
 	return (
 		<header className="hero-image">
 			<div className="hero-text">
-				<h1>WELCOME TO</h1>
+				<h1 className="animate__animated animate__fadeIn animate__slower">
+					WELCOME TO
+				</h1>
 				<h2>Rooster Grin</h2>
-				<button>testing</button>
+				<Button variant="contained" style={style}>
+					Learn More
+				</Button>
 			</div>
 		</header>
 	);
